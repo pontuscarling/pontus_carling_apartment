fa79b4587c02cca65ce45119783e117c59c786de
pontuscarling <149163925+pontuscarling@users.noreply.github.com>
2023-11-27 07:34:18 +0100

    Måndag2711:0730

--- Assets/Scripts/Paintable.cs
+++ Assets/Scripts/Paintable.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Paintable : MonoBehaviour
 {
     public GameObject brush;
+    public Camera renderCamera;
     public float brushSize = 0.00001f;
     public Vector3 offset;
+    public RenderTexture rTexture;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
+            renderCamera.enabled = true;
             var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if(Physics.Raycast(Ray, out hit))
@@ -20,5 +23,24 @@ public class Paintable : MonoBehaviour
                 go.transform.localScale = Vector3.one * brushSize;
             }
         }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Save();
+            Debug.Log("Sparar textur");
+            renderCamera.enabled = false;
+        }
     }
+    public void Save()
+    {
+        StartCoroutine(CoSave());
+    }
+    private IEnumerator CoSave()
+    {
+        yield return new WaitForEndOfFrame();
+        RenderTexture.active = rTexture;
+        var texture2D = new Texture2D(rTexture.width, rTexture.height);
+        texture2D.ReadPixels(new Rect(0,0, rTexture.width, rTexture.height),0,0);
+        texture2D.Apply();
+    }
+
 }
